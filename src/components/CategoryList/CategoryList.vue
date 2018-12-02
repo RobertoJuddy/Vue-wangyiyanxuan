e18eab51dd324ea98e5017cd9c9a2744f4c3a01e
beto <849885262@qq.com>
2018-12-02 22:01:40 +0800

    upadat

---
+++ src/components/CategoryList/CategoryList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="Category">
+    <ul class="CategoryList">
+      <li :class="{active : isShow === index}" v-for="(data, index) in dataNav.categoryL1List"
+          :key="index"
+          @click="changeIshow(index)"
+      >{{data.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+  export default {
+    data () {
+      return {
+        isShow : 0
+      }
+    },
+    computed : {
+      ...mapState(['dataNav'])
+    },
+    methods : {
+      changeIshow (index) {
+        this.isShow = index
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/mixins.styl'
+  .Category
+    width 1.63rem
+    .CategoryList
+      width 1.62rem
+      border-right 0.02rem solid #EDEDED
+      li
+        width 1.62rem
+        height 0.5rem
+        font-size 0.25rem
+        text-align: center
+        line-height 0.5rem
+        margin 0.358rem 0
+        &.active
+          border-left 0.03rem solid red
+          font-size 0.34rem
+          color red
+
+</style>
--- src/pages/Category/Category.vue
+++ src/pages/Category/Category.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>category</div>
+  <div class="Category">
+    <div class="CategoryHeader">
+      <i class="iconfont icon-search"></i>
+      <input type="text" placeholder="搜索商品, 共9714款好物"/>
+    </div>
+    <div class="CategoryMain">
+      <div class="CategoryLeft">
+        <CategoryList />
+      </div>
+      <div class="CategoryRight"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+  import CategoryList from "../../components/CategoryList/CategoryList";
   export default {
-    name: "Category"
+    name: "Category",
+    components: {CategoryList}
   }
 </script>
 
-<style scoped>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/mixins.styl'
+  .Category
+    width 7.5rem
+    height 13.34rem
+    .CategoryHeader
+      width 8.1rem
+      height 0.89rem
+      display flex
+      align-items center
+      justify-content center
+      position relative
+      border-bottom 0.03rem solid #EDEDED
+      input
+        width 6.87rem
+        height 0.51rem
+        background #EDEDED
+        font-size 0.3rem
+        text-align center
+        margin-right 0.55rem
+      .iconfont
+        position absolute
+        left 1.5rem
+        top 0.3rem
+
 
 </style>
